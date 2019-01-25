ec9a16eccc67a14cc69ba61243859f5fb4c13183
venkisangi <venkisangi@gmail.com>
2019-01-25 01:35:41 -0500

    Sample test proj

---
+++ ClassLibraryPractice/ClassLibraryPractice/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassLibraryPractice
+{
+    public class Class1
+    {
+        public static int Addition(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
---
+++ ClassLibraryPractice/ClassLibraryTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ClassLibraryTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ClassLibraryTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("02659e94-3769-47f3-8307-80f3a8b0dd50")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
